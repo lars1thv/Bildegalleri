1b3c7c7eaf789b5380f974792c1ec8ba36ab7261
lars1thv <102662802+lars1thv@users.noreply.github.com>
2022-04-22 10:23:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// Bildegalleri
+var tilbake = document.getElementById("tilbakeKnapp");
+var neste = document.getElementById("nesteKnapp");
+var description = document.getElementById("description");
+var bilde = document.getElementById("bilde");
+var currentImage = 0;
+
+var bilder = [
+  {
+  link: "img/Bildegalleri001.jpeg",
+  text: "Rondane 2021"
+},
+{
+  link: "img/Bildegalleri002.jpeg",
+  text: "Svenneby Drivhus"
+},
+{
+  link: "img/Bildegalleri003.jpeg",
+  text: "Nanjing 2018"
+},
+{
+  link: "img/Bildegalleri004.jpeg",
+  text: "Taormina Togstasjon"
+},
+{
+  link: "img/Bildegalleri005.jpeg",
+  text: "Alma, Isabel og Lars"
+},
+{
+  link: "img/Bildegalleri006.jpeg",
+  text: "Ferje fra Sicilia"
+}
+];
+
+
+tilbake.addEventListener("click", function(){
+  currentImage = currentImage + 1;
+  if(currentImage === bilder.length){
+    currentImage = 0;
+  }
+bilde.scr = bilder[currentImage].link;
+description.textContent = bilder[currentImage].text;
+});
+
+neste.addEventListener("click", function(){
+  currentImage = currentImage - 1;
+  if(currentImage === - 1){
+    currentImage = bilder.length - 1;
+  }
+bilde.scr = bilder[currentImage].link;
+description.textContent = bilder[currentImage].text
+});
